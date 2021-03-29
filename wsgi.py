c9610163d5ac266e82aa430aca65866e986cd8a3
Real Keks <gdjvvjfisnkd@gmail.com>
2021-03-29 20:09:22 +0300

    fix app.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
+from data import db_session
+from os.path import exists
+from os import mkdir
 from app import app
+
+
+if not exists('./db'):
+    mkdir('./db')
+db_session.global_init('db/base.db')
 
 if __name__ == "__main__":
     app.run()
